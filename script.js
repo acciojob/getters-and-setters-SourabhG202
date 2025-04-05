579f7171dc1fd9c40ee32b7a3ace2e83135cbd22
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-05 11:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ set age(newAge){
 
 class Student extends Person {
 	study(){
-		console.log(`$(this.name) is studying`);
+		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`$(this.name) is teaching`);
+		console.log(`${this.name} is teaching`);
 	}
 }
 
