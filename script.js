0741909c6c8969e45ea1fbbeddad26589d757162
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-05 11:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class Person {
 		this._name = name;
 		this._age = age;
 	}
-}
+
 get name(){
 	return this._name;
 }
@@ -14,6 +14,7 @@ get age(){
 set age(newAge){
 	this._age = newAge
 	
+}
 }
 
 class Student extends Person {
